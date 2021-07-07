2528cc7d44bf57d01b435fd030f767d8c274e4a4
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-07-07 18:04:49 +0700

    chore: update service

--- src/controllers/booking.schema.ts
+++ src/controllers/booking.schema.ts
@@ -1,15 +1,55 @@
 import { FastifySchema } from "fastify";
 
-const pingSchema: FastifySchema = {
+export const bookingSchema: FastifySchema = {
+  headers: {
+    type: 'object',
+    properties: {
+      authorization: { type: 'string' }
+    },
+    require: ['authorization']
+  },
+  body: {
+    type: 'object',
+    properties: {
+      jobId: { type: 'string' },
+      truckId: { type: 'string' },
+      requesterType: { type: 'string' },
+      accepterUserId: { type: 'string' },
+    },
+    require: ['jobId', 'truckId', 'accepterUserId', 'requesterType']
+  },
   response: {
     200: {
-      type: 'object',
-      properties: {
-        message: { type: 'string' },
-      },
+      type: 'number',
       additionalProperties: false
     }
   }
 }
 
-export default pingSchema
+
+export const bookingUpdateSchema: FastifySchema = {
+  headers: {
+    type: 'object',
+    properties: {
+      authorization: { type: 'string' }
+    },
+    require: ['authorization']
+  },
+  body: {
+    type: 'object',
+    properties: {
+      status: { type: 'string' }
+    }
+  },
+  params: {
+    quotationId: { type: 'string' }
+  },
+  response: {
+    200: {
+      type: 'number',
+      additionalProperties: false
+    }
+  }
+}
+
+
---
+++ src/repositories/booking.repository.ts
@@ -0,0 +1,29 @@
+import { FastifyInstance } from 'fastify';
+import { FastifyInstanceToken, getInstanceByToken } from 'fastify-decorators';
+import { FindManyOptions, Repository, UpdateManyOptions } from 'typeorm';
+import { Booking } from '../models'
+import * as  Types from '../controllers/booking.types'
+
+export default class BookingRepository {
+
+  private instance: FastifyInstance = getInstanceByToken(FastifyInstanceToken);
+
+  async insert(data: Types.FullPostBooking): Promise<any> {
+    const server: any = this.instance
+    const bookingRepository: Repository<Booking> = server?.db?.booking;
+    return bookingRepository.save(bookingRepository.create(data));
+  }
+
+  async find(options: FindManyOptions): Promise<any> {
+    const server: any = this.instance
+    const bookingRepository: Repository<Booking> = server?.db?.booking;
+    return bookingRepository.find(options);
+  }
+
+  async update(options: any): Promise<any> {
+    const server: any = this.instance
+    const bookingRepository: Repository<Booking> = server?.db?.booking;
+    return bookingRepository.save(bookingRepository.create(options));
+  }
+
+}
