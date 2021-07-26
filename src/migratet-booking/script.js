1100a4a30c379989bfe2e5b5c9c4ccb73ca5c369
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-07-26 23:41:12 +0700

    chore: update service

--- src/migratet-booking/script.js
+++ src/migratet-booking/script.js
@@ -515,6 +515,28 @@ GROUP BY listall.id, listall.user_id, listall.loading_datetime, listall.product_
 GROUP BY jc.id, jc.job_id, jc.carrier_id;
   `
 
+  const sqlCreateViewTripListAll = `CREATE VIEW vw_trip_all AS SELECT t.id,
+  jc.job_id,
+  t.job_carrier_id,
+  t.truck_id,
+  vwjob.product_type_id,
+  vwjob.product_name,
+  vwjob.price,
+  vwjob.price_type,
+  vwjob.truck_type,
+  vwjob.weight AS total_weight,
+  vwjob.required_truck_amount,
+  t.status,
+  json_build_object('name', vwjob.loading_address, 'dateTime', vwjob.loading_datetime, 'contactName', vwjob.loading_contact_name, 'contactMobileNo', vwjob.loading_contact_phone, 'lat', vwjob.loading_latitude, 'lng', vwjob.loading_longitude) AS "from",
+  vwjob.shipments,
+  vwjob.owner AS job_owner,
+  json_agg(json_build_object('id', t.id, 'truckId', t.truck_id, 'weight', COALESCE(t.weight, vwtruck.loading_weight), 'price', COALESCE(t.price, vwjob.price), 'priceType', t.price_type, 'status', t.status, 'bookingId', t.booking_id, 'truckType', vwtruck.truck_type, 'stallHeight', vwtruck.stall_height, 'createdAt', vwtruck.created_at, 'updatedAt', vwtruck.updated_at, 'approveStatus', vwtruck.approve_status, 'phoneNumber', vwtruck.owner ->> 'mobileNo'::text, 'registrationNumber', vwtruck.registration_number, 'workingZones', vwtruck.work_zone, 'owner', vwtruck.owner, 'tipper', vwtruck.tipper)) AS trips
+ FROM trip t
+   LEFT JOIN job_carrier jc ON jc.id = t.job_carrier_id
+   LEFT JOIN dblink('jobserver'::text, 'SELECT id, product_type_id, product_name, price, price_type, truck_type, weight, required_truck_amount, loading_address, loading_datetime, loading_contact_name, loading_contact_phone, loading_latitude, loading_longitude, owner, shipments FROM vw_job_list'::text) vwjob(id integer, product_type_id integer, product_name text, price numeric, price_type text, truck_type text, weight numeric, required_truck_amount integer, loading_address text, loading_datetime text, loading_contact_name text, loading_contact_phone text, loading_latitude text, loading_longitude text, owner jsonb, shipments jsonb) ON vwjob.id = jc.job_id
+   LEFT JOIN dblink('truckserver'::text, 'SELECT id, approve_status, loading_weight, registration_number, stall_height, quotation_number, tipper, truck_type, created_at, updated_at, carrier_id, owner, work_zone FROM vw_truck_list'::text) vwtruck(id integer, approve_status character varying, loading_weight numeric, registration_number text[], stall_height character varying, quotation_number integer, tipper boolean, truck_type integer, created_at timestamp without time zone, updated_at timestamp without time zone, carrier_id integer, owner jsonb, work_zone jsonb) ON vwtruck.id = t.truck_id
+GROUP BY t.id, t.job_carrier_id, jc.job_id, vwjob.product_type_id, vwjob.truck_type, vwjob.weight, vwjob.required_truck_amount, vwjob.loading_address, vwjob.loading_datetime, vwjob.loading_contact_name, vwjob.loading_contact_phone, vwjob.loading_latitude, vwjob.loading_longitude, vwjob.owner, vwjob.shipments, vwjob.product_name, vwjob.price, vwjob.price_type;`
+
   await connectNewDB.query(sqlCreateViewBooking);
   await connectNewDB.query(sqlCreateViewJobBookingTruckList);
   await connectNewDB.query(sqlCreateViewJobWithBookingId);
@@ -522,6 +544,7 @@ GROUP BY jc.id, jc.job_id, jc.carrier_id;
   await connectNewDB.query(sqlCreateViewMyJobNewList);
   await connectNewDB.query(sqlCreateViewTripInprogress);
   await connectNewDB.query(sqlCreateViewTripWithTruckDetail);
+  await connectNewDB.query(sqlCreateViewTripListAll);
   console.log("Finished")
   return true;
 }
